84780c2d8a2339451847e8b8b80f1b377bc57ec4
jfjara <jfjara@AT-4C0KLH2.atsistemas.lan>
2019-11-21 21:27:04 +0100

    Add Exam entity

--- src/main/java/com/world/jfjara/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/world/jfjara/service/impl/QuestionServiceImpl.java
@@ -5,19 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.world.jfjara.repository.QuestionsRepository;
+import com.world.jfjara.repository.ExamRepository;
 import com.world.jfjara.service.QuestionService;
+import com.world.jfjara.views.model.Exam;
 import com.world.jfjara.views.model.Question;
 
 @Service
 public class QuestionServiceImpl implements QuestionService {
 
 	@Autowired
-	private QuestionsRepository repository;
+	private ExamRepository examRepository;
 	
 	@Override
 	public void save(List<Question> questions) {
-		repository.saveAll(questions);		
+		Exam exam = new Exam();
+		exam.setQuestions(questions);
+		examRepository.save(exam);		
 	}
 
 }
